1c885afeb0b2d772aa3acc6c9c4c6c9f39375b3b
Karandeep Singh <karandeepjabbal@gmail.com>
2022-03-12 19:48:13 +0530

    Added Redis

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const axios = require("axios");
 const express = require('express');
 const app = express()
-const {getResult_id} = require('./modules/results');
-
+const {client, getResult_id} = require('./modules/results');
+const async = require("async");
 var port =  3000;
-
 global.results = {};  // global variable for all results
-global.cache = {};    // global cache variable   
 
 app.get("/",async (req, res) => {
     console.log(req.query.roll);
     let roll = req.query.roll;
-    
-        if(cache[roll] == 1){
-          console.log("From cache");
-            // console.log(results);
-            return res.send(results);
-        } 
-        cache[roll] = 1;
-        getResult_id(roll, res);
+    if(!roll){
+      return res.status(400).send({message:"Wrong Parameters"});
+    }   
+    getResult_id(roll, res);
 
 });
-app.get("/print", async(req,res)=>{
-    
+app.get("/cache",async (req, res) => {
+  console.log(req.query.roll);
+  let roll = req.query.roll;
+  let key = req.query.key;
+  data = {};
+  if(key !== undefined){
+    await client.hgetall(key, function (error, value) {
+      console.log(value);
+      if(!value){
+        return res.status(400).send({message:"No Data Found"});
+      }
+      data['message'] = "OK";       
+      data[key] = value;
+      return res.status(200).json(data);
+    });
+} 
+  else if(roll !== undefined){
+    await client.smembers(roll, function (error, value) {
+     if(!value){
+      return res.status(400).send({message:"No Data Found"}); 
+     }
+      data['message'] = "OK";
+     data[roll] = value;
+     console.log(value);
+     return res.status(200).json(data);
+  });  
+}  else {
+      res.status(400).send({message:"Wrong Parameters"});
+    }   
 });
+
   app.listen(port, () => {
     console.log(`Server Started at http://localhost:${port}`);
   });
